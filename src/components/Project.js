e752f9e537e031d1b9f4a978297be56ded3ec3bc
jsoonee <jsoonee12@gmail.com>
2021-10-19 23:08:22 +0900

    edit project section

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import tdl from '../images/tdl320.png';
 import weather from '../images/weather320.png';
+import shop from '../images/shop320.png';
 
 const Project = () => {
     return (
         <section className="sec even project" id="project">
             <h1>PROJECTS</h1>
             <div className="flexbox">
+
                 <a href="https://github.com/jsoonee/todolist" target="_blank">
                     <div className="frame">
                     
@@ -22,6 +24,7 @@ const Project = () => {
                         </div>
                     </div>
                 </a>
+
                 <a href="https://github.com/jsoonee/weather" target="_blank">
                     <div className="frame">
                         <div className="fflex">
@@ -30,7 +33,21 @@ const Project = () => {
                             </div>
                             <div className="des">
                                 <h2>실시간 날씨</h2>
-                                <p>전세계 도시별 날씨를 실시간으로 확인할 수 있는 간단한 앱</p>
+                                <p>전세계 도시의 실시간 날씨를 볼 수 있는 간단한 앱</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://github.com/jsoonee/sooneeshop" target="_blank">
+                    <div className="frame">
+                        <div className="fflex">
+                            <div className="pjs">
+                                <img src={shop} alt="shopping cart" className="scn"/>
+                            </div>
+                            <div className="des">
+                                <h2>가상 쇼핑몰</h2>
+                                <p>장바구니 추가/삭제, 필터 등의 기능을 탑재한 쇼핑몰</p>
                             </div>
                         </div>
                     </div>
