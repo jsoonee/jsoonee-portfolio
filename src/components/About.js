8b8f1c8defb0e61b68e90039cd0bb2a7fb0f4715
jsoonee <jsoonee12@gmail.com>
2021-10-20 23:15:40 +0900

    edit about

--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ const About = () => {
                                 </div>
                                 <div className="word">
                                     <p>새로운 일에 도전하는 것을 즐기는 27살 청년입니다.</p>
-                                    <p>저는 웹 프론트엔드, 특히 React와 같은 JavaScript 기반 프레임워크를 통해 쾌적한 UI/UX를 제공하는 것에 관심이 있습니다.</p>
+                                    <p>저는 웹 프론트엔드, 특히 React와 같은 JavaScript 기반 SPA를 통해 쾌적한 UI/UX를 제공하는 것에 관심이 있습니다.</p>
                                     <p>현실에 안주하지 않고 좋은 웹 개발을 위해 끊임없이 고민하겠습니다.</p>
                                 </div>
                             </div>
